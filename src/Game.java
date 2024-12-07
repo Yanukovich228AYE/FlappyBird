4342797b37a4f932351c4592eecf9aaaef942e2e
babage <72308592+Yanukovich228AYE@users.noreply.github.com>
2024-12-07 07:09:50 +0100

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game {
     JFrame window;
     JPanel cardPanel; // Panel with card layout
     CardLayout cardLayout; // card layout for switching screens
     GamePanel gamePanel; // game panel
+    JLabel points;
 
     public Game() {
         window = new JFrame("Flappy Bird");
@@ -19,6 +22,15 @@ public class Game {
 
         JPanel menuPanel = createMenuPanel(); // TODO
 
+        gamePanel = new GamePanel(this);
+
+        // points count
+        points = new JLabel();
+        points.setFont(new Font("Arial", Font.BOLD, 48));
+        points.setText("0");
+        points.setAlignmentX(Component.CENTER_ALIGNMENT);
+        gamePanel.add(points);
+
         cardPanel.add(menuPanel, "Menu");
         cardPanel.add(gamePanel, "Game");
 
@@ -28,6 +40,39 @@ public class Game {
     }
 
     private JPanel createMenuPanel() {
-        return new JPanel();
+        JPanel menuPanel = new JPanel();
+        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
+        menuPanel.setBackground(Color.gray);
+
+        JLabel title = new JLabel("Flappy Bird");
+        title.setFont(new Font("Arial", Font.BOLD, 48));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        menuPanel.add(title);
+        menuPanel.add(Box.createRigidArea(new  Dimension(0, 50))); // space between title and other components
+
+        JButton startButton = getjButton();
+        menuPanel.add(startButton);
+
+        return menuPanel;
+    }
+
+    private JButton getjButton() {
+        JButton startButton = new JButton("Start");
+        startButton.setBackground(Color.WHITE);
+        startButton.setFont(new Font("Arial", Font.PLAIN, 24));
+        startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "Game"); // Switch to the game panel
+                gamePanel.restartGame(); // Restart the game
+                gamePanel.requestFocusInWindow(); // Ensure the game panel has focus
+            }
+        });
+        return startButton;
+    }
+
+    public void showMenu() {
+        cardLayout.show(cardPanel, "Menu"); // Switch back to the menu panel
     }
 }
