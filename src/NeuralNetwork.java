89228e79b2aa636038b0388e650fa963eddb4a0d
babage <72308592+Yanukovich228AYE@users.noreply.github.com>
2024-12-20 13:17:38 +0100

    Update NeuralNetwork.java

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,9 +1,48 @@
 public class NeuralNetwork {
-    
-}
+    double[][] weightsIH;
+    double[][] weightsHO;
 
-class Neuron {
-    double value;
-    int nWeights;
-    int[] weights;
+    private double bias;
+
+    public NeuralNetwork(int numWeights) {
+        this.weights = new double[numWeights];
+        randomizeWeights();
+    }
+
+    public void randomizeWeights() {
+        for (int i = 0; i < weights.length; i++)
+            weights[i] = Math.random() * 2 - 1;
+
+        bias = Math.random() * 2 - 1;
+    }
+
+    public double feed(double[] inputs) {
+        double sum = 0;
+        for (int i = 0; i < inputs.length; i++) {
+            sum += inputs[i] * weights[i];
+        }
+
+        sum += bias;
+        return sigmoid(sum);
+    }
+
+    public double sigmoid(double x) {
+        return 1.0 / (1.0 + Math.exp(-x));
+    }
+
+    public double[] getWeights() {
+        return weights;
+    }
+
+    public void setWeights(double[] newWeights) {
+        this.weights = newWeights.clone();
+    }
+
+    public double getBias() {
+        return bias;
+    }
+
+    public void setBias(double b) {
+        this.bias = b;
+    }
 }
