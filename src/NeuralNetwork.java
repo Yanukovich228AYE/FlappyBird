c6d05611db9e3a0e6ce28d3f3278aa39982fdca2
babage <72308592+Yanukovich228AYE@users.noreply.github.com>
2024-12-20 13:45:34 +0100

    Update NeuralNetwork.java

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,48 +1,69 @@
+import java.util.Random;
+
 public class NeuralNetwork {
-    double[][] weightsIH;
-    double[][] weightsHO;
+    private double[][] weightsIH; // Input-to-Hidden layer weights
+    private double[][] weightsHO; // Hidden-to-Output layer weights
+    private double[] biasH;
+    private double[] biasO;
 
-    private double bias;
+    private Random rand = new Random();
 
-    public NeuralNetwork(int numWeights) {
-        this.weights = new double[numWeights];
+    public NeuralNetwork(int inputSize, int hiddenSize, int outputSize) {
+        weightsIH = new double[hiddenSize][inputSize];
+        weightsHO = new double[outputSize][hiddenSize];
+        biasH = new double[hiddenSize];
+        biasO = new double[outputSize];
+
         randomizeWeights();
     }
 
-    public void randomizeWeights() {
-        for (int i = 0; i < weights.length; i++)
-            weights[i] = Math.random() * 2 - 1;
-
-        bias = Math.random() * 2 - 1;
-    }
-
-    public double feed(double[] inputs) {
-        double sum = 0;
-        for (int i = 0; i < inputs.length; i++) {
-            sum += inputs[i] * weights[i];
+    private void randomizeWeights() {
+        // Initialize weights with small random values
+        for (int i = 0; i < weightsIH.length; i++) {
+            for (int j = 0; j < weightsIH[0].length; j++) {
+                weightsIH[i][j] = rand.nextGaussian() * 0.5;
+            }
+            biasH[i] = rand.nextGaussian() * 0.5;
         }
 
-        sum += bias;
-        return sigmoid(sum);
+        for (int i = 0; i < weightsHO.length; i++) {
+            for (int j = 0; j < weightsHO[0].length; j++) {
+                weightsHO[i][j] = rand.nextGaussian() * 0.5;
+            }
+        }
+        for (int i = 0; i < biasO.length; i++) {
+            biasO[i] = rand.nextGaussian() * 0.5;
+        }
     }
 
-    public double sigmoid(double x) {
+    public double feedForward(double[] inputs) {
+        // Calculate hidden layer
+        double[] hidden = new double[weightsIH.length];
+        for (int i = 0; i < weightsIH.length; i++) {
+            double sum = 0;
+            for (int j = 0; j < weightsIH[0].length; j++) {
+                sum += inputs[j] * weightsIH[i][j];
+            }
+            sum += biasH[i];
+            hidden[i] = sigmoid(sum);
+        }
+
+        // Calculate output layer
+        double[] output = new double[weightsHO.length];
+        for (int i = 0; i < weightsHO.length; i++) {
+            double sum = 0;
+            for (int j = 0; j < weightsHO[0].length; j++) {
+                sum += hidden[j] * weightsHO[i][j];
+            }
+            sum += biasO[i];
+            output[i] = sigmoid(sum);
+        }
+
+        // Since we have only one output neuron, return it
+        return output[0];
+    }
+
+    private double sigmoid(double x) {
         return 1.0 / (1.0 + Math.exp(-x));
-    }
-
-    public double[] getWeights() {
-        return weights;
-    }
-
-    public void setWeights(double[] newWeights) {
-        this.weights = newWeights.clone();
-    }
-
-    public double getBias() {
-        return bias;
-    }
-
-    public void setBias(double b) {
-        this.bias = b;
     }
 }
